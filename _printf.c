fe4c7186674636290bfd3622833580eb18a80d0a
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-11-22 00:37:28 -0800

    updated _printf.c

--- _printf.c
+++ _printf.c
@@ -27,7 +27,7 @@ int _printf(const char * const format, ...)
 Here:
 	while (format[i] != '\0')
 	{
-		j = 10;
+		j = 11;
 		while (j >= 0)
 		{
 			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
